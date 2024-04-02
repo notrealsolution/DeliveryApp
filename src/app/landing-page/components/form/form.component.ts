aece96df83442d2d3a551f13524d128be3c14816
Daniel Antonio Sánchez Jiménez <notrealsolution@gmail.com>
2024-04-02 08:14:20 -0600

    Iniciando con los estilos del formulario

--- src/app/landing-page/components/form/form.component.ts
+++ src/app/landing-page/components/form/form.component.ts
@@ -3,6 +3,6 @@ import { Component} from '@angular/core';
 @Component({
   selector: 'landing-page-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+  styleUrls: ['./form.component.scss']
 })
 export class FormComponent{}
